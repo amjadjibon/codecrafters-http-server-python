758cd9fc24af31a743df34d2e489566956fb1d12
Amjad Jibon <amjad.jibon@gmail.com>
2024-03-10 16:17:23 +0800

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 import socket
 
+def handle_connection(conn, addr):
+    print("Connected to", addr)
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.close()
+
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.listen(1)
+    
+    while True:
+        print("Waiting for a connection...")
+        conn, addr = server_socket.accept()
+        handle_connection(conn, addr)
 
 
 if __name__ == "__main__":
